48ddc466ff4e20f4799d694f519b6ea6c302aadb
VincentRssx <Vincent.rssx59@gmail.com>
2023-10-30 09:14:42 +0100

    modification for PR

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -3,13 +3,12 @@ import "./Card.scss";
 function Card() {
   return (
     <div className="Card">
-      {/* <img src={Croix} className="croix" alt="" /> */}
       <div className="planetDesc">
         <div className="Description">
-          <h1>Titre de la planètes</h1>
-          <p> Superbe Description de la planète !!! </p>
+          <h1>Planet title</h1>
+          <p> Description </p>
         </div>
-        <div className="planète">
+        <div className="planet">
           <img className="earth" src={Image} alt="" />
         </div>
       </div>
