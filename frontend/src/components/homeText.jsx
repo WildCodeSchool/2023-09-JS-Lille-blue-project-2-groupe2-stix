415df469a091c1e2d91ed45485413268c61552ab
Marie-Sail <marie.saillard.pro@gmail.com>
2023-10-30 12:21:31 +0100

    renam className

--- frontend/src/components/homeText.jsx
+++ frontend/src/components/homeText.jsx
@@ -4,20 +4,20 @@ import "./homeText.scss";
 function HomeText() {
   return (
     <div className="homeText">
-      <p className="text1">
+      <p className="resumGalaxy">
         Le <span className="textColor">système Solaire</span> se trouve dans la
         galaxie nommée <span className="textColor">Voie Lactée</span>
       </p>
-      <p className="text2">
+      <p className="resumSS1">
         Il est constitué d'une étoile, de huit planètes ainsi que de multiple
         petits astres, et météore.
       </p>
-      <p className="text3">
+      <p className="resumSS2">
         Les planètes
         <span className="textColor"> gravitent autour du soleil</span> sur leurs
         propres orbites.
       </p>
-      <p className="text4">
+      <p className="resumPlanet">
         Chaque planète possède
         <span className="textColor"> sa propre vitesse de déplacement </span>
         autour de l’étoile. Lorsque les planètes font un tour complet autour du
