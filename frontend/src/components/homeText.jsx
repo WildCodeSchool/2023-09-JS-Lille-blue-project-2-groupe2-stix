d3d25e9686dc8c0d4cd9a5838067e9404038d3c3
Marie-Sail <marie.saillard.pro@gmail.com>
2023-10-31 16:57:04 +0100

    animation text

--- frontend/src/components/homeText.jsx
+++ frontend/src/components/homeText.jsx
@@ -1,23 +1,51 @@
-import React from "react";
 import "./homeText.scss";
+import { useInView } from "framer-motion";
+import { useRef } from "react";
 
 function HomeText() {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
   return (
     <div className="homeText">
-      <p className="resumGalaxy">
+      <p
+        className="resumGalaxy"
+        ref={ref}
+        style={{
+          opacity: isInView ? 1 : 0,
+          transition: "all 1.5s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+        }}
+      >
         Le <span className="textColor">système Solaire</span> se trouve dans la
         galaxie nommée <span className="textColor">Voie Lactée</span>
       </p>
-      <p className="resumSS1">
+      <p
+        className="resumSS1"
+        style={{
+          opacity: isInView ? 1 : 0,
+          transition: "all 1.5s cubic-bezier(0.17, 0.55, 0.55, 1) 1s",
+        }}
+      >
         Il est constitué d'une étoile, de huit planètes ainsi que de multiple
         petits astres, et météore.
       </p>
-      <p className="resumSS2">
+      <p
+        className="resumSS2"
+        style={{
+          opacity: isInView ? 1 : 0,
+          transition: "all 1.5s cubic-bezier(0.17, 0.55, 0.55, 1) 1.5s",
+        }}
+      >
         Les planètes
         <span className="textColor"> gravitent autour du soleil</span> sur leurs
         propres orbites.
       </p>
-      <p className="resumPlanet">
+      <p
+        className="resumPlanet"
+        style={{
+          opacity: isInView ? 1 : 0,
+          transition: "all 1.5s cubic-bezier(0.17, 0.55, 0.55, 1) 2s",
+        }}
+      >
         Chaque planète possède
         <span className="textColor"> sa propre vitesse de déplacement </span>
         autour de l’étoile. Lorsque les planètes font un tour complet autour du
