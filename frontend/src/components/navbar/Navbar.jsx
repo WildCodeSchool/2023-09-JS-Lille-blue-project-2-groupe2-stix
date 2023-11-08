3e12cc1d02154648d2f834bea8bea4aa5d0ba30b
Marie-Sail <marie.saillard.pro@gmail.com>
2023-11-08 10:50:35 +0100

    interaction card et navbar

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,21 +1,29 @@
 import PropTypes, { shape } from "prop-types";
 import "./Navbar.scss";
 
-function Navbar({ systeme }) {
+function Navbar({ systeme, setIndexPlanet, setShowCard }) {
   return (
     <nav className="navbar">
       <div className="planets">
         {systeme &&
           systeme.map((planet) => {
             return (
-              <div className="planet" key={planet.id}>
+              <button
+                type="button"
+                onClick={() => {
+                  setIndexPlanet(planet.id);
+                  setShowCard(true);
+                }}
+                className="planet"
+                key={planet.id}
+              >
                 <img
                   className="planet-img"
                   src={`${import.meta.env.VITE_BACKEND_URL}${planet.image}`}
                   alt={planet.french_name}
                 />
                 <p>{planet.french_name}</p>
-              </div>
+              </button>
             );
           })}
       </div>
@@ -33,6 +41,8 @@ Navbar.propTypes = {
       type_de_planete: PropTypes.string.isRequired,
     }).isRequired
   ).isRequired,
+  setIndexPlanet: PropTypes.func.isRequired,
+  setShowCard: PropTypes.func.isRequired,
 };
 
 export default Navbar;
