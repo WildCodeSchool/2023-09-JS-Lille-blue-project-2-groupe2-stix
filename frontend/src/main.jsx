910501aaaf86067fe6410a7b6bc7782ba4e388fc
VincentRssx <Vincent.rssx59@gmail.com>
2023-11-14 10:26:32 +0100

    composant terminer

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-unresolved */
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
