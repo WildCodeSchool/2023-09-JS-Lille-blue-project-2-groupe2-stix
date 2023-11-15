59a6f9e41894e9edb944203d8c96c52b6a6f9db4
Marie-Sail <marie.saillard.pro@gmail.com>
2023-11-15 10:33:23 +0100

    changement de l'animation des texts

--- frontend/src/components/homeText/homeText.jsx
+++ frontend/src/components/homeText/homeText.jsx
@@ -1,55 +1,92 @@
-import React, { useRef } from "react";
 import "./homeText.scss";
-import { useInView } from "framer-motion";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
 
 function HomeText() {
-  const ref = useRef(null);
-  const isInView = useInView(ref, { once: true });
+  gsap.registerPlugin(ScrollTrigger);
+  gsap.fromTo(
+    ".resumGalaxy",
+    { opacity: 0 },
+    {
+      scrollTrigger: {
+        trigger: ".resumGalaxy",
+        start: "top 80%",
+        end: "bottom 80%",
+        toggleActions: "play complet reverse reset",
+      },
+      opacity: 1,
+      duration: 1,
+      x: -20,
+    }
+  );
+  gsap.fromTo(
+    ".resumSS1",
+    {
+      opacity: 0,
+    },
+    {
+      scrollTrigger: {
+        trigger: ".resumSS1",
+        start: "top 80%",
+        end: "bottom 80%",
+        toggleActions: "play complet reverse reset",
+      },
+      opacity: 1,
+      duration: 1,
+      x: -20,
+    }
+  );
+  gsap.fromTo(
+    ".resumSS2",
+    { opacity: 0 },
+    {
+      scrollTrigger: {
+        trigger: ".resumSS2",
+        start: "top 80%",
+        end: "bottom 80%",
+        toggleActions: "play complet reverse reset",
+      },
+      opacity: 1,
+      duration: 1,
+      x: -20,
+    }
+  );
+  gsap.fromTo(
+    ".resumPlanet",
+    { opacity: 0 },
+    {
+      scrollTrigger: {
+        trigger: ".resumPlanet",
+        start: "top 80%",
+        end: "bottom 80%",
+        toggleActions: "play complet reverse reset",
+      },
+      opacity: 1,
+      duration: 1,
+      x: -20,
+    }
+  );
   return (
     <div className="homeTexts">
-      <p
-        className="resumGalaxy "
-        ref={ref}
-        style={{
-          opacity: isInView ? 1 : 0,
-          transition: "all 1.5s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
-        }}
-      >
+      <p className="resumGalaxy ">
         Notre <span className="textColor">système solaire</span> se trouve dans
-        une galaxie nommée <span className="textColor">Voie lactée</span>
+        une galaxie nommée la<span className="textColor">Voie lactée</span>.
       </p>
-      <p
-        className="resumSS1"
-        style={{
-          opacity: isInView ? 1 : 0,
-          transition: "all 1.5s cubic-bezier(0.17, 0.55, 0.55, 1) 1s",
-        }}
-      >
-        Il est constitué d'une étoile, de huit planètes ainsi que d'une
+      <p className="resumSS1">
+        Il est constitué d'une étoile, de huit planètes, ainsi que d'une
         multitude d'astres et de météores.
       </p>
-      <p
-        className="resumSS2"
-        style={{
-          opacity: isInView ? 1 : 0,
-          transition: "all 1.5s cubic-bezier(0.17, 0.55, 0.55, 1) 1.5s",
-        }}
-      >
+      <p className="resumSS2">
         Les planètes
-        <span className="textColor"> gravite autour du soleil</span> sur son
+        <span className="textColor"> gravitent autour du soleil</span> sur leur
         propre orbite.
       </p>
-      <p
-        className="resumPlanet"
-        style={{
-          opacity: isInView ? 1 : 0,
-          transition: "all 1.5s cubic-bezier(0.17, 0.55, 0.55, 1) 2s",
-        }}
-      >
+      <p className="resumPlanet">
         Chaque planète possède
         <span className="textColor"> sa propre vitesse de déplacement </span>
-        autour de l’étoile. Lorsque les planètes font un tour complet autour du
-        soleil, cela se nomme une <span className="textColor">révolution</span>.
+        autour de l’étoile. Lorsque les planètes effectuent un tour complet
+        autour du soleil, cela se nomme une{" "}
+        <span className="textColor">révolution</span>.
       </p>
     </div>
   );
