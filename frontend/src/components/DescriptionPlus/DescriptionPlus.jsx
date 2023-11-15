aa96dd5dda2c97c7aeb928695606d3ae3224b7d4
Farid-Zian <130969864+Farid-Zian@users.noreply.github.com>
2023-11-15 09:42:25 +0100

    Fix import scss files

--- frontend/src/components/DescriptionPlus/DescriptionPlus.jsx
+++ frontend/src/components/DescriptionPlus/DescriptionPlus.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useParams } from "react-router-dom";
 import systeme from "../../../../backend/src/data.json";
+import "./DescriptionPlus.scss";
 
 function DescriptionPlus() {
   const { id } = useParams();
